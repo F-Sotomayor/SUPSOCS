56e8a79c136bf1e8e7d762ad1c5f2491861ccc65
F-Sotomayor <facundosotomayor95@hotmail.com>
2020-11-12 22:17:17 -0300

    asd

---
+++ theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/core"
+import { theme } from "@chakra-ui/theme"
+
+export default extendTheme( {
+    colors: {
+        primary: theme.colors.twitter,
+        secondary: theme.colors.gray,
+    }
+})
